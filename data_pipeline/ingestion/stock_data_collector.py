df93071ba1ee7c9c18ec2a038f11322e27c6d3e1
Meenu <meenumary12@gmail.com>
2025-03-24 01:58:44 +0100

    Updated the code

---
+++ data_pipeline/ingestion/stock_data_collector.py
@@ -0,0 +1,53 @@
+# Import necessary libraries
+
+import yfinance as yf # Fetches stock market data
+import yaml
+import time
+import pandas as pd
+
+# Configuration
+
+# Load from config.yaml
+with open("config.yaml", "r") as f:
+    cfg = yaml.safe_load(f)
+
+SYMBOL = cfg.get("symbol", "AAPL") # Stock ticker
+INTERVAL = cfg.get("interval", "15m") # Data frequency
+HISTORY_PERIOD = cfg.get("history_period", "60d") # Fetch last 60 days of historical data
+RECENT_PERIOD = cfg.get("recent_period", "1d") # Fetch recent 1 day data
+STREAMING_INTERVAL = int(cfg.get("streaming_interval", "900")) # 15 minutes in seconds
+OUTPUT_FILE = cfg.get("output_file", "stock_market_data.csv")
+
+# Main program
+
+# Fetch historical data
+def fetch_historical_data(ticker):
+    return ticker.history(period = HISTORY_PERIOD, interval = INTERVAL)
+    
+
+# Fetch latest data
+def fetch_recent_data(ticker):
+    return ticker.history(period = RECENT_PERIOD, interval = INTERVAL).tail(1)
+
+def main():
+    # Create a ticker object
+    ticker = yf.Ticker(SYMBOL)
+
+    historical_data = fetch_historical_data(ticker)
+    print("Historical data fetched", historical_data.head())
+
+    while True:
+        recent_data = fetch_recent_data(ticker)
+        print("Latest_data fetched", recent_data)
+    
+         # Ensure new data exists
+        if not recent_data.empty:
+            data = pd.concat([historical_data, recent_data]).drop_duplicates()
+            data.to_csv(OUTPUT_FILE, index=True)
+    else:
+        print("No new data fetched")
+    
+    time.sleep(STREAMING_INTERVAL)
+
+if __name__ == "__main__":
+    main()
