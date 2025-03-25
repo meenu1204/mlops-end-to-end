2b7867a76b9d6edca344aa9b2b3ce4d08a1dfc77
Meenu <meenumary12@gmail.com>
2025-03-26 00:24:43 +0100

    Updated code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Project title : <b style="font-size:24px;">MLOps-driven Stock Market Prediction</b>
+1. Project title : <b style="font-size:24px;">MLOps-driven Stock Market Prediction</b>
+2. Goal: Implement a stock trading decision system. It should:
+    a. Predict Buy/Sell signals
+    b. Decide how much to trade
+    c. Predict Profit or Loss
+    d. Adopt MLOps practices
+
--- data_pipeline/ingestion/stock_data_collector.py
+++ data_pipeline/ingestion/stock_data_collector.py
@@ -3,7 +3,15 @@
 import yfinance as yf # Fetches stock market data
 import yaml
 import time
+import logging
 import pandas as pd
+
+# logging
+logging.basicConfig(
+    filename='streaming.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 # Configuration
 
@@ -15,7 +23,7 @@ SYMBOL = cfg.get("symbol", "AAPL") # Stock ticker
 INTERVAL = cfg.get("interval", "15m") # Data frequency
 HISTORY_PERIOD = cfg.get("history_period", "60d") # Fetch last 60 days of historical data
 RECENT_PERIOD = cfg.get("recent_period", "1d") # Fetch recent 1 day data
-STREAMING_INTERVAL = int(cfg.get("streaming_interval", "900")) # 15 minutes in seconds
+STREAMING_INTERVAL = int(cfg.get("streaming_interval", "86400")) # 1 day in seconds
 OUTPUT_FILE = cfg.get("output_file", "stock_market_data.csv")
 
 # Main program
@@ -34,20 +42,21 @@ def main():
     ticker = yf.Ticker(SYMBOL)
 
     historical_data = fetch_historical_data(ticker)
-    print("Historical data fetched", historical_data.head())
+    logging.info(f"Historical data fetched for {SYMBOL}, head: {historical_data.head()}")
 
     while True:
         recent_data = fetch_recent_data(ticker)
-        print("Latest_data fetched", recent_data)
+        logging.info(f"Latest data fetched: {recent_data}")
     
          # Ensure new data exists
         if not recent_data.empty:
             data = pd.concat([historical_data, recent_data]).drop_duplicates()
             data.to_csv(OUTPUT_FILE, index=True)
-    else:
-        print("No new data fetched")
+            logging.info(f"New data written to {OUTPUT_FILE} at {recent_data.index[0]}")
+        else:
+            logging.warning("No new data fetched")
     
-    time.sleep(STREAMING_INTERVAL)
+        time.sleep(STREAMING_INTERVAL)
 
 if __name__ == "__main__":
     main()
