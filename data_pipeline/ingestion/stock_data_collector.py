59a0f0e28c8e0cc96c7dc373697fd72efc005b89
Meenu <meenumary12@gmail.com>
2025-03-28 14:52:26 +0100

    updated logic

--- data_pipeline/ingestion/stock_data_collector.py
+++ data_pipeline/ingestion/stock_data_collector.py
@@ -1,37 +1,47 @@
+# File name: stock_data_collector
+# Collect historical and recent data for a specific ticker ans store it to a .csv file
+
 # Import necessary libraries
 
 import yfinance as yf # Fetches stock market data
 import yaml
-import time
-import logging
 import pandas as pd
+import logging
+import time
+from datetime import datetime, time as et_time
+from zoneinfo import ZoneInfo
 
-# logging
+
+# logging setup
 logging.basicConfig(
     filename='streaming.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-# Configuration
-
-# Load from config.yaml
+# Load configuration
 with open("config.yaml", "r") as f:
     cfg = yaml.safe_load(f)
 
-SYMBOL = cfg.get("symbol", "AAPL") # Stock ticker
-INTERVAL = cfg.get("interval", "15m") # Data frequency
-HISTORY_PERIOD = cfg.get("history_period", "60d") # Fetch last 60 days of historical data
-RECENT_PERIOD = cfg.get("recent_period", "1d") # Fetch recent 1 day data
-STREAMING_INTERVAL = int(cfg.get("streaming_interval", "86400")) # 1 day in seconds
-OUTPUT_FILE = cfg.get("output_file", "stock_market_data.csv")
+SYMBOL = cfg["symbol"] # Stock ticker
+INTERVAL = cfg["interval"] # Data frequency
+HISTORY_PERIOD = cfg["history_period"] # Fetch last 60 days of historical data
+RECENT_PERIOD = cfg["recent_period"] # Fetch recent 1 day data
+STREAMING_INTERVAL = int(cfg["streaming_interval"]) # 1 day in seconds
+OUTPUT_FILE = cfg["output_file"]
 
-# Main program
+# Ensure fetching of recent data during US market hours
+def is_market_open():
+    eastern = ZoneInfo("US/Eastern")
+    now_et = datetime.now(tz=eastern)
+    market_open = et_time(9,30)
+    market_close = et_time(16,00)
+    return now_et.weekday() < 5 and market_open <= now_et.time() <= market_close
+
 
 # Fetch historical data
 def fetch_historical_data(ticker):
     return ticker.history(period = HISTORY_PERIOD, interval = INTERVAL)
-    
 
 # Fetch latest data
 def fetch_recent_data(ticker):
@@ -40,23 +50,24 @@ def fetch_recent_data(ticker):
 def main():
     # Create a ticker object
     ticker = yf.Ticker(SYMBOL)
-
     historical_data = fetch_historical_data(ticker)
     logging.info(f"Historical data fetched for {SYMBOL}, head: {historical_data.head()}")
 
     while True:
-        recent_data = fetch_recent_data(ticker)
-        logging.info(f"Latest data fetched: {recent_data}")
+        if is_market_open():
+            recent_data = fetch_recent_data(ticker)
+            logging.info(f"Latest data fetched: {recent_data}")
     
-         # Ensure new data exists
-        if not recent_data.empty:
-            data = pd.concat([historical_data, recent_data]).drop_duplicates()
-            data.to_csv(OUTPUT_FILE, index=True)
-            logging.info(f"New data written to {OUTPUT_FILE} at {recent_data.index[0]}")
+            # Ensure new data exists
+            if not recent_data.empty:
+                data = pd.concat([historical_data, recent_data]).drop_duplicates()
+                data.to_csv(OUTPUT_FILE, index=True)
+                logging.info(f"New data written to {OUTPUT_FILE} at {recent_data.index[0]}")
+            else:
+                logging.info("No new data fetched")
         else:
-            logging.warning("No new data fetched")
-    
+            logging.info("Market closed. Skipping data fetch")
         time.sleep(STREAMING_INTERVAL)
-
+# Entry
 if __name__ == "__main__":
-    main()py
+    main()
