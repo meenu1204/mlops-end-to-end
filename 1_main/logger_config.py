dc29448541b106417f889299fb0c2f89caf55e8d
Meenu <meenumary12@gmail.com>
2025-04-01 15:24:23 +0200

    Updated logger_config.py

--- 1_main/logger_config.py
+++ 1_main/logger_config.py
@@ -6,16 +6,16 @@ import logging
 from logging import Logger, FileHandler, StreamHandler, Formatter
 from typing import Optional
 
-def get_logger(name:Optional[str] = None, log_file: str = "main.log") -> Logger:
+def get_logger(name:Optional[str] = None, log_file: str = 'main.log') -> Logger:
     logger = logging.getLogger(name)
     if not logger.hasHandlers():
         logger.setLevel(logging.INFO)
         
         file_handler = FileHandler(log_file)
-        file_handler.setFormatter(Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+        file_handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 
         console_handler = FileHandler(log_file)
-        console_handler.setFormatter(Formatter("%(name)s - %(levelname)s - %(message)s"))
+        console_handler.setFormatter(Formatter('%(name)s - %(levelname)s - %(message)s'))
 
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
